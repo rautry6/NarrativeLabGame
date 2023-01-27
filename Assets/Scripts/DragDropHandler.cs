0ae352adbe308ef2a56ca5f139f8faa9de4c6af6
rautry6 <94204738+rautry6@users.noreply.github.com>
2023-01-27 17:31:08 -0500

    Drag and Drop Functionality
    Added basic Drag and Drop Functionality in Test Scene

---
+++ Assets/Scripts/DragDropHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDropHandler : MonoBehaviour
+{
+    public int numRejected = 0;
+
+   
+}
---
+++ Assets/Scripts/Resume.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Resume : MonoBehaviour, IDropHandler
+{
+
+    [SerializeField] private DragDropHandler dragDropHandler;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null && dragDropHandler.numRejected < 3)
+        {
+            GetComponentInChildren<TMP_Text>().color = eventData.pointerDrag.GetComponent<TMP_Text>().color;
+            dragDropHandler.numRejected++;
+        }
+    }
+}

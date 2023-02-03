8cb65f3beb30fdeabe39746a102925aa2b8d7840
rautry6 <94204738+rautry6@users.noreply.github.com>
2023-02-03 12:48:25 -0500

    Resume Format Changes
    New Font, Restructuring of Text Fields, Font Changes, Spacing tweaks

--- Assets/Scripts/Resume/SelectedResume.cs
+++ Assets/Scripts/Resume/SelectedResume.cs
@@ -6,13 +6,13 @@ using UnityEngine.EventSystems;
 
 public class SelectedResume : MonoBehaviour
 {
-    [SerializeField] private TMP_Text displayText;
+    [SerializeField] private TMP_Text[] displayText;
     [SerializeField] private ResumeHolder[] resumes;
     [SerializeField] private int currentResume = 0;
     // Start is called before the first frame update
     void Awake()
     {
-        displayText = GetComponentInChildren<TMP_Text>();
+
     }
 
     // Update is called once per frame
@@ -23,14 +23,13 @@ public class SelectedResume : MonoBehaviour
 
     public void ShowResume(Resume resume)
     {
-        displayText.text =
-            "Name: " + resume.applicant_name + "\n" + 
-            "DOB: " + resume.applicant_age + "\n" +
-            "Sex: " + resume.applicant_sex + "\n" +
-            "Race: " + resume.applicant_race + "\n" +
-            "Skills: " + resume.applicant_skills + "\n" +
-            "Work History: " + resume.applicant_work_history + "\n" +
-            "Education: " + resume.applicant_education;
+        displayText[0].text = "NAME: " + resume.applicant_name;
+        displayText[1].text = "DOB: " + resume.applicant_age;
+        displayText[2].text = "SEX: " + resume.applicant_sex;
+        displayText[3].text = "RACE: " + resume.applicant_race;
+        displayText[4].text = "SKILLS: " + resume.applicant_skills;
+        displayText[5].text = "WORK HISTORY: " + resume.applicant_work_history;
+        displayText[6].text = "EDUCATION: " + resume.applicant_education;
 
         currentResume = resume.resumeNum;
     }

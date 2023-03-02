9449da4663331efe9f0e40b1002eab906bd4304a
M-d-Wilson <61097691+M-d-Wilson@users.noreply.github.com>
2023-03-02 00:29:49 -0500

    can move through resumes
    made a script that handles the movement, enabling, and disabling of scripts.

--- Assets/Scripts/Resume/ResumeHolder.cs
+++ Assets/Scripts/Resume/ResumeHolder.cs
@@ -12,6 +12,8 @@ public class ResumeHolder : MonoBehaviour, IDropHandler, IPointerDownHandler
     [SerializeField] public int resumeNum;
     [SerializeField] public Resume resume;
     [SerializeField] private GameObject rejectMark;
+    [SerializeField] private TMP_Text nameText, ageText, sexText, raceText, skillsText,
+        workHistoryText, educationText, likesText, dislikesText;
 
     public string applicant_name;
     public string applicant_age;
@@ -25,7 +27,7 @@ public class ResumeHolder : MonoBehaviour, IDropHandler, IPointerDownHandler
 
     private void Awake()
     {
-        
+        FillResume();
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -45,5 +47,28 @@ public class ResumeHolder : MonoBehaviour, IDropHandler, IPointerDownHandler
         Debug.Log("OnPointerDown");
         selectedResume.SetActive(true);
         selectedResume.GetComponent<SelectedResume>().ShowResume(resume);
+    }
+
+    public void FillResume()
+    {
+        //did this because I need its
+        if (applicant_name != null && nameText != null)
+            nameText.text = applicant_name;
+        if (applicant_age != null && ageText != null)
+            ageText.text = applicant_age;
+        if (applicant_sex != null && sexText != null)
+            sexText.text = applicant_sex;
+        if (applicant_race != null && raceText != null)
+            raceText.text = applicant_race;
+        if (applicant_skills != null && skillsText != null)
+            skillsText.text = applicant_skills;
+        if (applicant_work_history != null && workHistoryText != null)
+            workHistoryText.text = applicant_work_history;
+        if (applicant_education != null && educationText != null)
+            educationText.text = applicant_education;
+        if (applicant_likes != null && likesText != null)
+            likesText.text = applicant_likes;
+        if (applicant_dislikes != null && dislikesText != null)
+            dislikesText.text = applicant_dislikes;
     }
 }
---
+++ Assets/Scripts/Resume/ResumeStack.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeStack : MonoBehaviour
+{
+    [Range(1, 100)]
+    [SerializeField] private int numberOfResumes = 1;
+    [SerializeField] private GameObject resumePrefab;
+    [SerializeField] private GameObject spawnPoint;
+    [Space]
+    [SerializeField] private List<GameObject> resumes;
+    [SerializeField] int listPos;
+    [Range(1, 10)]
+    [SerializeField] float slideSpeed = 1;
+
+    Vector2 oGPos, slideStart;
+    //[SerializeField] 
+    float moveTime;
+    //[SerializeField] 
+    bool moveResume;
+
+
+    void Start()
+    {
+        slideStart = oGPos = spawnPoint.transform.position;
+        slideStart -= new Vector2(300, 0);
+        PopulateRseumes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveResume)
+        {
+            moveTime += Time.deltaTime;
+
+            if (resumes[listPos].transform.position.x != oGPos.x)
+            {
+                if (moveTime > 1)
+                {
+                    moveTime = 1;
+                    moveResume = false;
+                }
+                float x = Mathf.Lerp(slideStart.x, oGPos.x, moveTime * slideSpeed);
+                resumes[listPos].transform.position = new Vector2( x, resumes[listPos].transform.position.y);
+            }
+        }
+    }
+
+    private void PopulateRseumes()
+    {
+        ResumeHolder resScript;
+        GameObject currentResume;
+        //Clears array if it has anything in it
+        if (resumes.Count > 0)
+        {
+            for (int i = 0; i < resumes.Count; i++)
+                Destroy(resumes[i]);
+            resumes.Clear();
+        }
+
+        //Fills array
+        for(int i = 0; i < numberOfResumes; i++)
+        {
+            currentResume = Instantiate(resumePrefab, spawnPoint.transform);
+            resumes.Add(currentResume);
+            resScript = resumes[i].GetComponent<ResumeHolder>();
+            resScript.applicant_name = "Ms." + i;
+            resScript.applicant_age = i + " Years old";
+            resScript.applicant_work_history = "Worked at Deez for " + i + " years";
+            resScript.applicant_education = "Graduated form " + i + " University";
+            resScript.FillResume();
+            currentResume.SetActive(false);
+        }
+        resumes[0].SetActive(true);
+        listPos = 0;
+    }
+
+    public void NextInList()
+    {
+        if (listPos == resumes.Count - 1)
+        {
+            //If at end we go to the end of
+            resumes[listPos].SetActive(false);
+            listPos = 0;
+            resumes[listPos].SetActive(true);
+            MovePage(resumes[listPos]);
+        }
+        else
+        {
+            //Else gose to next resume in array
+            resumes[listPos].SetActive(false);
+            listPos++;
+            resumes[listPos].SetActive(true);
+            MovePage(resumes[listPos]); 
+        }
+    }
+
+    public void PrevInList()
+    {
+        if (listPos == 0)
+        {
+            //If at 0 we go to the end of
+            resumes[listPos].SetActive(false);
+            listPos = resumes.Count - 1;
+            resumes[listPos].SetActive(true);
+            MovePage(resumes[listPos]);
+        }
+        else
+        {
+            //Else gose to previous resume in array
+            resumes[listPos].SetActive(false);
+            listPos--;
+            resumes[listPos].SetActive(true);
+            MovePage(resumes[listPos]);
+        }
+    }
+
+    public void GoToInList(int resumeX)
+    {
+        for (int i = 0; i < resumes.Count; i++)
+            resumes[i].SetActive(false);
+        listPos = resumeX;
+        resumes[listPos].SetActive(true);
+        MovePage(resumes[listPos]);
+    }
+
+    private void MovePage(GameObject resume)
+    {
+        resume.transform.position = slideStart;
+        moveResume = true;
+        moveTime = 0;
+    }
+}
